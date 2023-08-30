8c69d321eb81a3901053b21a0f9900dd25969cda
haidar180202 <gshaidar6@gmail.com>
2023-08-30 14:58:13 +0700

    initialization section 1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar-laravel-1 || About</title>
+</head>
+<body>
+    <h2>Halaman About</h2>
+    <h4><?= $name ?></h4>
+    <p><?= $email ?></p>
+    <img src="img/<?= $image ?>" alt="pasfoto">
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Muhammad Haidar Shahab",
+        "email" => "gshaidar6@gmail.com",
+        "image" => "pasfoto.jpg"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');
 });
